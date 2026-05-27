d4346741b0b69944bc1d0009038fcb9e7409e256
OFFERSHAHAR <poolsys1@gmail.com>
2026-05-27 15:36:16 +0300

    Configure live Sheets script URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.AFTER_CLASS_AI_CONFIG = {
-  sheetsApiUrl: "",
+  sheetsApiUrl: "https://script.google.com/macros/s/AKfycbxN6EKwE8-EVQROfMfYa4dMB666gy_5pXCpNLS1tB9t5-1RSl3u8YVi1UEK8L2Gyqwq_g/exec",
   defaultSessionCode: "AI-203",
   refreshSeconds: 10
 };
